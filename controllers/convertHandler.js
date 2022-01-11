9c49e6c3172f7542a1039144c83298baa404e8b8
Mark Gauffin <markgauffin@gmail.com>
2022-01-11 06:55:31 -0500

    add getNum and getUnit functions

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,65 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
-  };
+    this.getNum = function (input) {
+        const string = input.toString();
+        const numRegex = /^\d+(\.\d+)?|^\.\d+/;
+        let match = string.match(numRegex);
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
-  };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
-  };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
+        if (match) {
+            return parseFloat(match[0]);
+        }
+        return false;
+    };
+
+    this.getUnit = function (input) {
+        const string = input.toString();
+        const gal = string.match(/gal/i);
+        const l = string.match(/l/i);
+        const lbs = string.match(/lbs/i);
+        const kg = string.match(/kg/i);
+        const mi = string.match(/mi/i);
+        const km = string.match(/km/i);
+
+        return gal
+            ? 'gal'
+            : l
+            ? 'l'
+            : lbs
+            ? 'lbs'
+            : kg
+            ? 'kg'
+            : mi
+            ? 'mi'
+            : km
+            ? 'km'
+            : false;
+    };
+
+    this.getReturnUnit = function (initUnit) {
+        let result;
+
+        return result;
+    };
+
+    this.spellOutUnit = function (unit) {
+        let result;
+
+        return result;
+    };
+
+    this.convert = function (initNum, initUnit) {
+        const galToL = 3.78541;
+        const lbsToKg = 0.453592;
+        const miToKm = 1.60934;
+        let result;
+
+        return result;
+    };
+
+    this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+        let result;
+
+        return result;
+    };
 }
 
 module.exports = ConvertHandler;
