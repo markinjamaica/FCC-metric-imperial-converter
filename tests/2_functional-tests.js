3743d22ea4581531facda4b24324fdd1b57ba8f3
Mark Gauffin <markgauffin@gmail.com>
2022-01-15 06:30:20 -0500

    test(functional): add assertion to test convert a valid input

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -11,12 +11,18 @@ suite('Functional Tests', function () {
         chai.request(server)
             .get('/api/convert?input=10l')
             .end((err, res) => {
+                assert.equal(
+                    res.text,
+                    '{"initNum":10,"initUnit":"l","returnNum":2.64172,"returnUnit":"gal","string":"10 liters converts to 2.64172 gallons"}'
+                );
                 assert.equal(res.status, 200);
                 done();
             });
     });
 
     // 2. convert an invalid input such as 32g: GET request to /api/convert
+    // test('convert an invalid input', done);
+
     // 3. convert an invalid number such as 3/7.2/4kg: GET request to /api/convert
     // 4. convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert
     // 5. convert with no number such as kg: GET request to /api/convert
