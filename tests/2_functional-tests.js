b41dd59a7f8b71055ff8913bea9aab962525c282
Mark Gauffin <markgauffin@gmail.com>
2022-01-15 06:04:24 -0500

    test(functional): convert a valid input

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,19 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
+    // 1. convert a valid input such as 10L: GET request to /api/convert
+    test('convert a valid input', (done) => {
+        chai.request(server)
+            .get('/api/convert?input=10l')
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                done();
+            });
+    });
 
+    // 2. convert an invalid input such as 32g: GET request to /api/convert
+    // 3. convert an invalid number such as 3/7.2/4kg: GET request to /api/convert
+    // 4. convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert
+    // 5. convert with no number such as kg: GET request to /api/convert
 });
